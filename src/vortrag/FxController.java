49fae2de58d65ddf63975ffea109541fccc02f70
hebner <hebner.tec@gmail.com>
2015-01-18 11:29:56 +0100

    FXML Version ohne Trennung View Controller
    - FXML View file erstellt
    - Controler Klasse erstellt
    - Start Klasse erstellt die das Fenster erzeugt usw.

---
+++ src/vortrag/FxController.java
@@ -0,0 +1,22 @@
+package vortrag;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+
+public class FxController {
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private Text greeting;
+	
+	@FXML
+	protected void handleSubmitAction(final ActionEvent e){
+		greeting.setText("Hello, " + username.getText());
+	}
+}
---
+++ src/vortrag/SignIn_fxml.java
@@ -0,0 +1,22 @@
+package vortrag;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SignIn_fxml extends Application{
+	@Override
+	public void start(Stage s) throws Exception {
+		final Parent root = FXMLLoader.load(getClass().getResource("singIn_fxml.fxml"));
+		
+		s.setScene(new Scene(root));
+		s.setTitle("Sign in via fxml");
+		s.show();
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+}
